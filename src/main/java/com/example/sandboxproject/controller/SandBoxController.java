29dcf0c1fe535d65f446774d312cd2bdc65c3c2e
kimyoungjae <newy12@naver.com>
2022-03-09 14:42:08 +0900

    샌드박스과제제출

--- src/main/java/com/example/sandboxproject/controller/SandBoxController.java
+++ src/main/java/com/example/sandboxproject/controller/SandBoxController.java
@@ -7,9 +7,11 @@ import com.example.sandboxproject.entity.Profit;
 import com.example.sandboxproject.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@RequestMapping(value = "/sandbox/exam")
 @RestController
 @RequiredArgsConstructor
 public class ApiController {
